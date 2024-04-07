b17764018d244559d5bf43c46a7d780348f3dbff
xuanw <62654668+BERA-wx@users.noreply.github.com>
2024-04-07 11:18:01 +0800

    Add files via upload

--- contrastive_loss/SupervisedContrastiveLoss.py
+++ contrastive_loss/SupervisedContrastiveLoss.py
@@ -1,3 +1,31 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0e825d27a46e0d030b3cbf44c314271711d30b7b246dd56dbb63c4c050793ae9
-size 1182
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SupConLoss(nn.Module):
+    def __init__(self, temp: list):
+        super(SupConLoss, self).__init__()
+        self.temperature = temp[0]
+        self.base_temperature = temp[1]
+
+    def forward(self, features, labels):
+        device = features.device
+        features = F.normalize(features, p=2, dim=1)
+        feats_dot_feats = torch.div(
+            torch.matmul(features, features.t()),
+            self.temperature
+        )
+        logits_max, _ = torch.max(feats_dot_feats, dim=1, keepdim=True)
+        logits = feats_dot_feats - logits_max.detach()
+
+        mask = (labels.unsqueeze(0) == labels.unsqueeze(1)).float().to(device)
+        logits_mask = 1 - torch.eye(logits.shape[0], logits.shape[1], device=device)
+        mask = mask * logits_mask
+
+        exp_logits = torch.exp(logits) * logits_mask
+        log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+        mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+        loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+        loss = loss.mean()
+        return loss
